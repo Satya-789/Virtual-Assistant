701407c4f1ad6b47acdff88b6544dc12e20839df
Satya Prakash mohanty <104158437+Satya-789@users.noreply.github.com>
2026-03-28 21:17:13 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -6,7 +6,7 @@ import os
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain_groq import ChatGroq
 
 # Load env
